37d4250dde906f6aa27c7a8747a5fd55b769c6a8
aleksey-rezvov <aleksey.rezvov@gmail.com>
2020-04-20 09:15:01 +0000

    Deploying to gh-pages from  @ b200d6aab0342f00d078614fcb3b31c286996182 🚀

---
+++ precache-manifest.e5cbcb2e1b695894457890ada265f4a2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccbf63ac11fee309a603c1c85e8e228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aab189a832bd38ec7a49",
+    "url": "/static/css/2.c4265262.chunk.css"
+  },
+  {
+    "revision": "1a61c9c77e7432a755e5",
+    "url": "/static/css/main.bce8376c.chunk.css"
+  },
+  {
+    "revision": "aab189a832bd38ec7a49",
+    "url": "/static/js/2.bfdc5e3f.chunk.js"
+  },
+  {
+    "revision": "68775c764ec02ab147e712f952efccb9",
+    "url": "/static/js/2.bfdc5e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a61c9c77e7432a755e5",
+    "url": "/static/js/main.87c9c7d7.chunk.js"
+  },
+  {
+    "revision": "efedfc98d8e4e4af49e7",
+    "url": "/static/js/runtime-main.d2f04a06.js"
+  },
+  {
+    "revision": "6e8c2eaece8612c3d11a8667c00de76f",
+    "url": "/static/media/envelope-body-front.6e8c2eae.png"
+  },
+  {
+    "revision": "29d383945b0390e03123c609a4d1bf70",
+    "url": "/static/media/envelope-flip.29d38394.png"
+  },
+  {
+    "revision": "2726696a2be0f17a226ee48f14a70030",
+    "url": "/static/media/intro.2726696a.webm"
+  },
+  {
+    "revision": "f045493132e820502c0ba2f9e40843d9",
+    "url": "/static/media/white-paperboard.f0454931.png"
+  }
+]);
